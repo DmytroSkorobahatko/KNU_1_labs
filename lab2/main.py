51818483bb3147166fd5484f11c03f58b193d1bd
Dmitriy <dmitriy180623@gmail.com>
2021-11-14 08:58:22 +0200

    lab2 pre_completed

--- lab2/main.py
+++ lab2/main.py
@@ -28,34 +28,50 @@ def _introduction_():
           'This program calculates the value of the series for lab2. Variant %d.' % variant, sep='\n')
 
 
-def _checker_x_(x):
+def _checker_x_(x: float):
     """checks if x in domain """
     if not _domain_x_(x):
         raise ValueError
 
 
-def _checker_eps(eps):
+def _checker_eps(eps: float):
     """checks if eps in domain"""
     if not _domain_eps_(eps):
         raise ValueError
 
 
-def _calculations_(x, eps):
+def _calculations_(x: float, eps: float) -> float:
     """starts calculation, calls checker"""
     print("***** do calculations ... ", end="")
     return s(x, eps)
 
 
-def _domain_x_(x):
+def _domain_x_(x: float):
+    """domain for x"""
     return a <= x <= b
 
 
-def _domain_eps_(eps):
+def _domain_eps_(eps: float):
+    """domain for eps"""
     return eps > 0
 
 
-def s(x, eps):
-    return 13.131313000
+def s(x: float, eps: float) -> float:
+    """
+    Обчислення функції s з точністю eps.
+    """
+    a = -1 / (7 * 11) * (math.pow(x, 11))
+    s = a
+    n = 2
+    while math.fabs(a) <= eps:
+        a *= math.pow(-1, n) / ((4 * n + 3) * (4 * n + 7)) * math.pow(x, 4 * n + 7)
+        prev_s = s
+        s += a
+        n += 1
+        if prev_s == s:
+            break
+
+    return float(s)
 
 
 if __name__ == '__main__':
