4cd8c60c2fb493735653a7c65bef473eebd30999
Dmitriy <dmitriy180623@gmail.com>
2021-11-15 11:47:28 +0200

    lab2 PreFinal Version

--- lab2/main.py
+++ lab2/main.py
@@ -1,24 +1,37 @@
-import math
-
 A, B = -0.9, 0.9
 
 
 def main():
     """catches excepts, calls introduction and calculations"""
     _introduction_()
+    inc_error = ""
+    v_error = ""
+    error_text = "'There are no input or it could not be converted to float'"
     try:
+        v_error = "'At time of getting x'"
         x = float(input("Enter a real from [-0.9; 0.9]: "))
-        _checker_x_(x)
+        if not _domain_x_(x):
+            inc_error = ", 'Incorrect value'"
+            stopper = float("")
+
+        v_error = "'At time of getting eps'"
         eps = float(input("Enter positive eps: "))
-        _checker_eps(eps)
-        res = _calculations_(x, eps)
+        if not _domain_eps_(eps):
+            inc_error = ", 'Incorrect value'"
+            stopper = float("")
+
+        res = _calculation_(x, eps)
         print("done", f"for x = {x:.5f}", f"for eps = {eps:.4e}", f"result = {res:.9f}", sep='\n')
-    except ValueError:
-        print("***** error")
+    except ValueError as ve:
+        ve_error = f", ValueError({error_text}, ValueError('{ve}'))" if inc_error == "" else ""
+        print("***** error", f"\n({v_error}{inc_error}{ve_error})")
     except KeyboardInterrupt:
         print("program aborted")
-    except EOFError:
-        print("***** error")
+    except EOFError as eof:
+        eof_error = f", EOFError({error_text}, EOFError('{eof}'))" if inc_error == "" else ""
+        print("***** error", f"\n({v_error}{eof_error})")
+    else:
+        print("The work is done.")
 
 
 def _introduction_():
@@ -28,25 +41,13 @@ def _introduction_():
           'This program calculates the value of the series for lab2. Variant %d.' % variant, sep='\n')
 
 
-def _checker_x_(x: float):
-    """checks if x in domain """
-    if not _domain_x_(x):
-        raise ValueError
-
-
-def _checker_eps(eps: float):
-    """checks if eps in domain"""
-    if not _domain_eps_(eps):
-        raise ValueError
-
-
-def _calculations_(x: float, eps: float) -> float:
+def _calculation_(x: float, eps: float) -> float:
     """starts calculation, calls checker"""
     print("***** do calculations ... ", end="")
     return s(x, eps)
 
 
-def _domain_x_(x: float):
+def _domain_x_(x: float) -> bool:
     """domain for x"""
     return A <= x <= B
 
@@ -57,21 +58,18 @@ def _domain_eps_(eps: float):
 
 
 def s(x: float, eps: float) -> float:
-    """
-    Обчислення функції s з точністю eps.
-    """
-    a = -1 / (7 * 11) * (math.pow(x, 11))
+    """calculates example that depends on x and eps"""
+    x4 = x * x * x * x
+    a = -1 * x4 / (7 / -1)
     s = a
-    n = 2
-    while math.fabs(a) <= eps:
-        a *= math.pow(-1, n) / ((4 * n + 3) * (4 * n + 7)) * math.pow(x, 4 * n + 7)
-        prev_s = s
+    n = 1
+    m_eps = eps * -1
+    while a >= eps or a <= m_eps:
+        a *= -1 * x4 / ((4 * n + 7) / (4 * n - 1))
         s += a
         n += 1
-        if prev_s == s:
-            break
-
-    return float(s)
+    else:
+        return s
 
 
 if __name__ == '__main__':
