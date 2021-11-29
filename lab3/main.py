0b7e45a7090682631806b4ae94b9bdb60b227669
Dmitriy <dmitriy180623@gmail.com>
2021-11-29 10:40:19 +0200

    lab3 PreCompleted

--- lab3/main.py
+++ lab3/main.py
@@ -4,77 +4,61 @@ This program for lab3. Variant 24
 """
 
 
-def main():
-    """catches excepts, calls introduction and some stuff"""
-    #input_str = input()
-    sequence = _get_sequence_(input_str)
-    sequence = _delete_trash_(sequence)
-
-    answer = subsequence(sequence)
+def subsequence(tuple_sequence):
+    """calls some stuff"""
+    return _plytka_(tuple_sequence)
 
 
-def subsequence(sequence):
+def _plytka_(sequence):
     """looks for First MaxLen parna plytka in string"""
     ans, temp = [], []
     pre_n = sequence[0]
-
+    n = 0
     for i in range(len(sequence)):
-        temp.append(sequence[i])
+        if _not_trash_(int(sequence[i])):
+            n += 1
+            temp.append(sequence[i])
 
-        if i % 2 == 0 and sequence[i] > pre_n:
-            temp = [sequence[i]]
-        if i % 2 == 1 and sequence[i] < pre_n:
-            temp = [sequence[i]]
+            if (n % 2 == 0 and sequence[i] > pre_n) or (n % 2 == 1 and sequence[i] < pre_n):
+                temp, n = [sequence[i]], 0
 
-        if len(ans) < len(temp):
-            ans = []
-            [ans.append(x) for x in temp]
+            if len(ans) < len(temp):
+                ans = []
+                [ans.append(x) for x in temp]
 
-        pre_n = sequence[i]
-
-    if len(ans) < 2:
-        ans = []
-    elif ans[-2] < ans[-1]:
-        ans.pop()
-
-    return ans
+            pre_n = sequence[i]
+    return _parna_(ans)
 
 
-def _get_sequence_(input_str):
-    """transform input to list of ints"""
-    sequence = []
-    for element in input_str.split():
-        sequence.append(int(element))
+def _parna_(sequence):
+    """makes our plytka parna"""
+    if len(sequence) < 2:
+        sequence = []
+    elif sequence[-2] < sequence[-1]:
+        sequence.pop()
     return sequence
 
 
-def _delete_trash_(sequence):
-    """removes a number without odd digit"""
-    new_sequence = []
-    for num in sequence:
-        not_odd = 0
+def _not_trash_(num: int):
+    """check if num include odd digits"""
+    not_odd = 0
+    if num < 0:
+        num *= -1
+    num_base7 = int(_convert_(num))
 
-        num_base7 = int(_convert_(num))
-        for digit in str(num_base7):
-            if int(digit) % 2 == 0:
-                not_odd += 1
-        if not_odd != len(str(num_base7)):
-            new_sequence.append(num)
-    return new_sequence
+    for digit in str(num_base7):
+        if int(digit) % 2 == 0:
+            not_odd += 1
+    return not_odd != len(str(num_base7))
 
 
 def _convert_(num, base=7):
+    """convert num to base 7"""
     alpha = "0123456789"
     converted = ""
     while num > 0:
         converted += alpha[num % base]
         num //= base
-
     if len(converted) == 0:
         return "0"
     return converted[::-1]
-
-
-"""program starts here"""
-if __name__ == '__main__':
-    main()
